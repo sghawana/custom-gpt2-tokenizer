3791ad5e02b298453394f1d590bb553156d65e86
sghawana <59884048+sghawana@users.noreply.github.com>
2024-06-28 05:12:44 +0530

    vocab completed

--- src/word2vec/corpus.py
+++ src/word2vec/corpus.py
@@ -1,5 +1,5 @@
 import os
-def generate_corpus(directory_path: str, create_txt_file: bool = True) -> (str):
+def generate_corpus(directory_path: str) -> (str):
     
     book_list = []
     for folder, _, files in os.walk(directory_path):  
@@ -14,11 +14,10 @@ def generate_corpus(directory_path: str, create_txt_file: bool = True) -> (str):
         with open(path, 'r', encoding='utf-8', errors='ignore') as f: temp = f.read()
         text_corpus += temp
         
-    if create_txt_file:
-        from paths import SOURCE
-        file_name = 'text_corpus.txt'
-        destination = os.path.join(SOURCE, file_name)
-        with open(destination, 'w', encoding='utf-8') as f:
-            f.write(text_corpus)
-            
-    return text_corpus
+    from paths import SOURCE
+    file_name = 'text_corpus.txt'
+    destination = os.path.join(SOURCE, file_name)
+    with open(destination, 'w', encoding='utf-8') as f:
+        f.write(text_corpus)
+    
+    del text_corpus
